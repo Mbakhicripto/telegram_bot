67051ab085a42a3c96f695145d7443183c7e0a8c
Mbakhicripto <143274272+Mbakhicripto@users.noreply.github.com>
2025-04-21 00:54:02 +0330

    Fix async await issue

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
+from telegram.ext import Application, CommandHandler, ContextTypes
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import asyncio
 import os
+import asyncio
 
-TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! من روی Render هستم :)")
 
 async def main():
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    await app.run_polling()
+    application = Application.builder().token(BOT_TOKEN).build()
+    application.add_handler(CommandHandler("start", start))
+    
+    await application.initialize()
+    await application.start()
+    await application.updater.start_polling()
+    await application.updater.idle()
 
-if __name__ == "__main__":
+    # وقتی کار تموم شد:
+    await application.stop()
+    await application.shutdown()
+
+if __name__ == '__main__':
     asyncio.run(main())
