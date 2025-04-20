66bc3300013ba6431813309c455d0d511c1d4ddb
Mbakhicripto <143274272+Mbakhicripto@users.noreply.github.com>
2025-04-21 00:35:23 +0330

    Add files via upload
    first commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import asyncio
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام! من روی Render هستم :)")
+
+async def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    await app.run_polling()
+
+if __name__ == "__main__":
+    asyncio.run(main())
